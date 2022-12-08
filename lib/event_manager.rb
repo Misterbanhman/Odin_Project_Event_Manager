b9c536d94def76466ff5bf398767c71e92537fef
misterbanhman <nathan.i.banh@gmail.com>
2022-12-07 20:26:12 -0700

    follow tutorial for file io

---
+++ lib/event_manager.rb
@@ -0,0 +1,22 @@
+require 'csv'
+
+puts "Event Manager Initialized!\n\n"
+
+def clean_zipcode(zipcode)
+    zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+contents = CSV.open(
+    'event_attendees.csv',
+    headers: true,
+    header_converters: :symbol
+)
+
+contents.each do |row|
+    name = row[:first_name]
+    zipcode = row[:zipcode]
+    
+    zipcode = clean_zipcode(zipcode)
+
+    puts "#{name} #{zipcode}"
+end
